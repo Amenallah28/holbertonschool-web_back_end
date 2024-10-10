9ac65b9a8137cacb12a703f90482c4943ad99e2c
Amenallah28 <amenallahmrabet9@gmail.com>
2024-10-10 17:39:40 +0100

    Extract Base 64 Authorization Header

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,13 @@ from typing import TypeVar
 class BasicAuth(Auth):
     """basic authenticatin class
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extract Base 64 Authorization Header"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return (authorization_header[6:])
+    
