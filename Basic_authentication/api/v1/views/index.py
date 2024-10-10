09b48442c0622abc61eb282c86043a4f759b490a
Amenallah28 <amenallahmrabet9@gmail.com>
2024-10-10 15:00:39 +0100

    indexes

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -25,9 +25,10 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/forbidden', methods=['GET'],strict_slashes=False )
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
-    """raises a 403 error by using abort
+    """ GET /api/v1/forbidden
+    Return:
+      - raises a 403 error by using abort
     """
     abort(403)
-    
