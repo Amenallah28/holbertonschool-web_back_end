491dfa4170d3e9f7ae263e0efa8aff1e4645df0b
Amenallah28 <amenallahmrabet9@gmail.com>
2024-10-10 14:58:56 +0100

    changed the function name

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,18 +14,18 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """
-    forbidden handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
 
 
 
