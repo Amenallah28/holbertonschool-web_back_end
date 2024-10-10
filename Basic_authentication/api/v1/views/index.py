3899038fc0cf6c93f06e731eeebdc844f9a67ca5
Amenallah28 <amenallahmrabet9@gmail.com>
2024-10-10 14:23:38 +0100

    raises a 403 abort error

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/forbidden', methods=['GET'],strict_slashes=False )
+def forbidden() -> str:
+    """raises a 403 error by using abort
+    """
+    abort(403)
+    
