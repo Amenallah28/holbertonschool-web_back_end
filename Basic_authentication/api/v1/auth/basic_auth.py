0ce3a91a570192fc8730d51530087d28e326a21f
Amenallah28 <amenallahmrabet9@gmail.com>
2024-10-10 17:30:42 +0100

    basic authentication class

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module of Basic Authentication
+"""
+from api.v1.auth.auth import Auth
+from typing import TypeVar
+
+class BasicAuth(Auth):
+    """basic authenticatin class
+    """
+    pass
