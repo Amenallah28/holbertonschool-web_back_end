017f918474575342b0d004cfa7bebd9748a5f155
Amenallah28 <amenallahmrabet9@gmail.com>
2024-10-14 19:53:36 +0100

    registre user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,7 +3,10 @@
 Methods to handle the apps
 authentication and login related functions
 """
+from db import DB
 import bcrypt
+from user import Base, User
+from sqlalchemy.orm.exc import NoResultFound
 
 def _hash_password(password: str) -> bytes:
     """
@@ -19,3 +22,23 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Method to register a new user in the database
+        """
+        try:
+            registered_user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            registered_user = self._db.add_user(email, hashed_password)
+            return registered_user
+        else:
+            raise ValueError
