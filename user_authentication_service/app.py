bd38cf272a89480156509a7ebf06dbb5419cc88f
Amenallah28 <amenallahmrabet9@gmail.com>
2024-10-14 22:08:23 +0100

    registre user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,11 @@
 """
 Module with the app's endpoints (routes)
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -11,7 +15,19 @@ def home():
     """return a json pauload with a welcome message"""
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=["POST"])
+def register_user():
+    """Post /users route to registre a new user"""
+    email = request.form.get("email")
+    paswword = request.form.get("paswword")
+
+    try:
+        user = AUTH.register_user(email, paswword)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registred"}), 400
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-    
+
