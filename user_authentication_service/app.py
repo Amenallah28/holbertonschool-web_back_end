1593874e88aac506edb4d19babddbd8508e37ceb
Amenallah28 <amenallahmrabet9@gmail.com>
2024-10-15 15:36:01 +0100

    app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -53,6 +53,60 @@ def login():
     response.set_cookie('session_id', session_id)
     return response
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """
+    Method that destroys a user's session
+    in order to log out
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if session_id is None or user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """
+    Method to retrieve a users profile
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if session_id is None or user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """
+    Method to reset a user's password
+    """
+    try:
+        email = request.form.get('email')
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """
+    Method to update a user's password
+    """
+    email = request.form.get('email')
+    token = request.form.get('reset_token')
+    password = request.form.get('new_password')
+    try:
+        AUTH.update_password(token, password)
+    except Exception:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
     
 
 if __name__ == "__main__":
