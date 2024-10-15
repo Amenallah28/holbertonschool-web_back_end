396e803497dbdc2238b44f51213b657586757af2
Amenallah28 <amenallahmrabet9@gmail.com>
2024-10-15 15:35:09 +0100

    auth

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -89,5 +89,38 @@ class Auth:
         except NoResultFound:
             return None
         
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Method that updates the session to None
+        in order to destroy it
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Mehtod to that generates an id and
+        resests the user's token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Method to update a user's password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        pwd = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=pwd, reset_token=None)
         
         
