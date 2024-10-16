ccbb99a68dad3760030843c63d459ece8324f458
Amenallah28 <amenallahmrabet9@gmail.com>
2024-10-16 12:59:22 +0100

    import redirect

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Module with the app's endpoints (routes)
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
